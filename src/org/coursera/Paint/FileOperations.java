83b93eb61706b996d0d9d0a4de543a9042365a8f
Pavel <klev.paul@gmail.com>
2014-12-01 09:55:30 +0300

    Initialization

---
+++ src/org/coursera/Paint/FileOperations.java
@@ -0,0 +1,182 @@
+package org.coursera.Paint;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.net.Uri;
+import android.os.Environment;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Copyright (c) 2014 Lawrence Angrave
+ */
+public class FileOperations {
+    private static final String TAG = FileOperations.class.getSimpleName();
+    private static final float ROTATE_ANGLE = 90;
+
+
+    public static void saveImage(Context context, Bitmap bitmap){
+        if (bitmap == null) {
+            return;
+        }
+        File path = Environment
+                .getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        Log.d(TAG, "saveAndShare path = " + path);
+        path.mkdirs();
+
+        // Note, for display purposes
+        // SimpleDateFormat.getTimeInstance()
+        // getDateTimeInstance() or getDateIntance
+        // are more appropriate.
+        // For filenames we can use the following specification
+        String timestamp = new SimpleDateFormat("yyyyMMdd_HHmmss")
+                .format(new Date());
+
+        String filename = "Image_" + timestamp + ".jpg";
+        // Alternatively ... use System.currentTimeMillis()
+
+        // Creating a new File object in Java does not create a new
+        // file on the device. The file object just represents
+        // a location or path that may or may not exist
+        File file = new File(path, filename);
+        FileOutputStream stream;
+        try {
+            // This can fail if the external storage is mounted via USB
+            stream = new FileOutputStream(file);
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream);
+            Toast.makeText(context, context.getString(R.string.ok_save_message) + filename, Toast.LENGTH_LONG).show();
+            stream.close();
+
+            Uri uri = Uri.fromFile(file);
+
+            // Tell Android that a new public picture exists
+            Intent intent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+            intent.setData(uri);
+            context.sendBroadcast(intent);
+
+
+        } catch (Exception e) {
+            Log.e(TAG, "saveAndShare (compressing):", e);
+            Toast.makeText(context, context.getString(R.string.problem_save_message) + filename, Toast.LENGTH_LONG).show();
+            return; // Do not continue
+        }
+
+    }
+    public static Bitmap loadImage(Context context, Intent data){
+        Uri uri = data.getData();
+        return loadImage(context,uri);
+
+
+    }
+
+    public static Bitmap RotateBitmap(Bitmap source, float angle)
+    {
+        Log.d(TAG, "Rotation");
+        Matrix matrix = new Matrix();
+        matrix.postRotate(angle);
+        return Bitmap.createBitmap(source, 0, 0, source.getWidth(), source.getHeight(), matrix, true);
+    }
+
+
+    public static Bitmap loadImage(Context context, Uri uri){
+        Log.d(TAG, uri.toString());
+        Toast.makeText(context, uri.toString(),
+                Toast.LENGTH_SHORT).show();
+
+        try {
+            InputStream stream = context.getContentResolver().openInputStream(uri);
+
+            BitmapFactory.Options options = new BitmapFactory.Options();
+            options.inJustDecodeBounds = true;
+
+            BitmapFactory.decodeStream(stream, null, options);
+            stream.close();
+
+            int w = options.outWidth;
+            int h = options.outHeight;
+            Log.d(TAG, "Bitmap raw size:" + w + " x " + h);
+
+            int displayW = context.getResources().getDisplayMetrics().widthPixels;
+            int displayH = context.getResources().getDisplayMetrics().heightPixels;
+
+
+
+            // check if need to rotate screen
+            if ((h > w && displayH < displayW) || (h < w && displayH > displayW)) {
+                // rotate screen
+                changeScreenOrientation(context);
+                //switch height and with
+                int temp = displayW;
+                displayW = displayH;
+                displayH = temp;
+            }
+
+
+
+            int sample = 1;
+
+            while (w > displayW * sample || h > displayH * sample) {
+                sample = sample * 2;
+            }
+            Log.d(TAG, "Sampling at " + sample);
+
+            options.inJustDecodeBounds = false;
+            options.inSampleSize = sample;
+
+            stream = context.getContentResolver().openInputStream(uri);
+            Bitmap bm = BitmapFactory.decodeStream(stream, null, options);
+            stream.close();
+
+
+            return bm;
+
+        } catch (Exception e) {
+            Log.e(TAG, "Decoding Bitmap", e);
+            Toast.makeText(context, context.getString(R.string.problem_load_message) +uri.toString(),
+                    Toast.LENGTH_LONG).show();
+            return null;
+        }
+
+
+    }
+
+    public static void changeScreenOrientation(Context context){
+        Activity activity = (Activity) context;
+        int orientation = context.getResources().getConfiguration().orientation;
+        switch(orientation) {
+            case Configuration.ORIENTATION_PORTRAIT:
+                activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+                break;
+            case Configuration.ORIENTATION_LANDSCAPE:
+                activity.setRequestedOrientation (ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+                break;
+        }
+    }
+
+    public static void forceScreenOrientation(Context context){
+        Activity activity = (Activity) context;
+        int orientation = context.getResources().getConfiguration().orientation;
+        switch(orientation) {
+            case Configuration.ORIENTATION_PORTRAIT:
+                activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+                break;
+            case Configuration.ORIENTATION_LANDSCAPE:
+                activity.setRequestedOrientation (ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+                break;
+        }
+    }
+}
---
+++ src/org/coursera/Paint/Painter.java
@@ -0,0 +1,287 @@
+package org.coursera.Paint;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.*;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import yuku.ambilwarna.AmbilWarnaDialog;
+
+
+public class Painter extends Activity implements View.OnTouchListener {
+    private static final String TAG = Painter.class.getSimpleName();
+    private static final String KEY_PAINT_COLOR = "p_color";
+    private static final String KEY_PAINT_SIZE = "p_size";
+    private static final String KEY_BACK_COLOR = "b_color";
+
+    private static final int DEF_BACK_COLOR = 0xffffffff; // White
+    private static final int DEF_PAINT_COLOR = 0xff000099; // Blue
+    private static final int DEF_PAINT_SIZE = 10;
+    private static final int MAX_PAINT_SIZE = 64;
+    private static final int REQUEST_CODE = 12345;
+
+    // this enum will store objects that color possible to change
+    private static enum coloredObj {BACKGROUND, BRUSH};
+
+    private SharedPreferences mPrefs;
+
+    private Bitmap mBitmap;
+    private Canvas mCanvas;
+    private ImageView mImageView;
+
+    private Paint mPaint;
+    private int mBackColor;
+    // These variables will store previous touch point coordinates
+    private float mX, mY;
+
+
+
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.main);
+        mImageView = (ImageView) findViewById(R.id.imageView);
+
+        //force screen orientation
+        FileOperations.forceScreenOrientation(this);
+
+        // Get size of the screen
+        View layout = findViewById(R.id.linearLayout);
+        int width = getResources().getDisplayMetrics().widthPixels;
+        int height = getResources().getDisplayMetrics().heightPixels - getStatusBarHeight();
+
+
+
+        Log.d(TAG, "Height:" + height + ", width:" + width);
+
+        // create bitmap & canvas
+        mBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+        mCanvas = new Canvas(mBitmap);
+        mCanvas.drawColor(DEF_BACK_COLOR);
+
+        mPaint = new Paint();
+        mPaint.setStyle(Paint.Style.FILL);
+
+
+        // From top left to bottom right
+        mPrefs = getPreferences(MODE_PRIVATE);
+        loadPreferences();
+
+
+        mImageView.setOnTouchListener(this);
+
+        //
+    }
+    //return height of status bar. It is where battery charge is shown
+    public int getStatusBarHeight() {
+        int result = 0;
+        int resourceId = getResources().getIdentifier("status_bar_height", "dimen", "android");
+        if (resourceId > 0) {
+            result = getResources().getDimensionPixelSize(resourceId);
+        }
+        return result;
+    }
+
+    //These two methods change background
+    private void setBackground(int color){
+        mBackColor = color;
+        mCanvas.drawColor(mBackColor);
+        // Update image
+        mImageView.setImageBitmap(mBitmap);
+    }
+    private void setBackground(Bitmap bitmap){
+        mBitmap.recycle();
+        mCanvas = null;
+
+        mBitmap = Bitmap.createBitmap(bitmap.getWidth(), bitmap.getHeight(),
+                Bitmap.Config.ARGB_8888);
+        mCanvas = new Canvas(mBitmap);
+        mCanvas.drawBitmap(bitmap, 0, 0, null);
+
+        bitmap.recycle();
+
+        // Update image
+        mImageView.setImageBitmap(mBitmap);
+
+    }
+
+    // These two methods deal with user preferences
+    private void loadPreferences(){
+        setBackground(mPrefs.getInt(KEY_BACK_COLOR, DEF_BACK_COLOR));
+        mPaint.setColor(mPrefs.getInt(KEY_PAINT_COLOR, DEF_PAINT_COLOR));
+        mPaint.setStrokeWidth(mPrefs.getInt(KEY_PAINT_SIZE, DEF_PAINT_SIZE));
+    }
+    private void savePreferences(){
+        SharedPreferences.Editor editor = mPrefs.edit();
+        editor.putInt(KEY_BACK_COLOR, mBackColor);
+        editor.putInt(KEY_PAINT_COLOR, mPaint.getColor());
+        editor.putInt(KEY_PAINT_SIZE, (int) mPaint.getStrokeWidth());
+        editor.commit();
+    }
+
+    private void openImageGallery(){
+        // Do some Intent magic to open the Gallery? Yes!
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("image/*");
+        startActivityForResult(
+                Intent.createChooser(intent, "Select..."), REQUEST_CODE);
+    }
+
+    @Override
+    public void onPause(){
+        savePreferences();
+        super.onPause();
+    }
+    @Override
+    public boolean onTouch(View view, MotionEvent motionEvent) {
+        //motionEvent.
+        float x = motionEvent.getX() * motionEvent.getXPrecision();
+        float y = motionEvent.getY() * motionEvent.getYPrecision();
+        // also possible to use these lines
+        //float x = motionEvent.getRawX();
+        //float y = motionEvent.getRawY();
+
+        switch (motionEvent.getAction()) {
+            case MotionEvent.ACTION_DOWN: // press
+                Log.d(TAG, "Begin x:" + x + ", y:" + y);
+                break;
+
+            case MotionEvent.ACTION_MOVE: // move
+                // Draw line and update image
+                mCanvas.drawLine(mX, mY, x, y, mPaint);
+                break;
+            case MotionEvent.ACTION_UP: // release
+            case MotionEvent.ACTION_CANCEL:
+                Log.d(TAG, "End x:" + x + ", y:" + y);
+                break;
+        }
+
+        // Save positions
+        mX = x;
+        mY = y;
+
+        //Draw a circle @ the end to nicer look
+        mCanvas.drawCircle(x, y, mPaint.getStrokeWidth()/2, mPaint);
+        // Update image
+        mImageView.setImageBitmap(mBitmap);
+        return true;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // when new backround returned
+        if (requestCode == REQUEST_CODE && resultCode == RESULT_OK) {
+            Bitmap bm = FileOperations.loadImage(this, data);
+            if (bm != null){
+                setBackground(bm);
+            }
+        }
+
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.brush_color:
+                pickColor(mPaint.getColor(), coloredObj.BRUSH);
+                return true;
+            case R.id.brush_size:
+                pickSize((int) mPaint.getStrokeWidth());
+                return true;
+            case R.id.background_solid_color:
+                pickColor(mBackColor, coloredObj.BACKGROUND);
+                return true;
+            case R.id.save:
+                FileOperations.saveImage(this, mBitmap);
+                return true;
+            case R.id.load:
+                openImageGallery();
+                return true;
+            case R.id.reset:
+                Intent intent = getIntent();
+                finish();
+                startActivity(intent);
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    // Show size picking dialog
+    private void pickSize(int initialSize){
+        final SeekBar sB = new SeekBar(this);
+        sB.setMax(MAX_PAINT_SIZE);
+        sB.setProgress(initialSize);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        // Add the buttons
+        builder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                // User clicked OK button
+                mPaint.setStrokeWidth(sB.getProgress());
+            }
+        });
+        builder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                // User cancelled the dialog
+                //Do nothing
+            }
+        });
+
+        // Set other dialog properties
+        builder.setTitle(R.string.size_dialog_title)
+                .setView(sB);
+
+        // Create and show the AlertDialog
+        builder.create().show();
+
+    }
+
+    //Show color picking dialog
+    private void pickColor(int initialColor, final coloredObj cObj){
+        // initialColor is the initially-selected color to be shown in the rectangle on the left of the arrow.
+        // for example, 0xff000000 is black, 0xff0000ff is blue. Please be aware of the initial 0xff which is the alpha.
+        AmbilWarnaDialog dialog = new AmbilWarnaDialog(this, initialColor, true, new AmbilWarnaDialog.OnAmbilWarnaListener() {
+            @Override
+            public void onOk(AmbilWarnaDialog dialog, int color) {
+                // color is the color selected by the user.
+                switch (cObj){
+                    case BACKGROUND:
+                        setBackground(color);
+                        break;
+                    case BRUSH:
+                        mPaint.setColor(color);
+                        break;
+                }
+
+            }
+
+            @Override
+            public void onCancel(AmbilWarnaDialog dialog) {
+                // cancel was selected by the user
+            }
+        });
+
+        dialog.show();
+    }
+
+}
